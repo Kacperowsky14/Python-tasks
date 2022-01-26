bf6171bead40085e593316cbd91e150cb694dd40
Kacper <93926070+Kacperowsky14@users.noreply.github.com>
2022-01-26 20:41:15 +0100

    Fibonacci sequence vol.1

--- fibonacci sequence.py
+++ fibonacci sequence.py
@@ -25,20 +25,10 @@ while z == 1:
         w = y+x
         print("the ", t, " number is",w,) #6,9,12,15...n
         t += 1
-        if t == 31 or t == 61 or t == 91 or t == 121 or t == 151 or t == 181 or t == 211:
-            z = int(input("Keep counting? Yes-1 No-0 \n"))
-            while z != 0 and z != 1:
-                print("Wrong command!")
-                z=int(input("Let's try again \n"))
-        if t == 241:
-            print("My memory can't take anymore ...")
-            time.sleep(1)
-            z = "dafuuuuuq"
+        z = int(input("Keep counting? Yes-1 No-0 \n"))
+        while z != 0 and z != 1:
+            print("Wrong command!")
+            z=int(input("Let's try again \n"))
 if z == 0:
     print("Bye!")
     time.sleep(1)
-if z == "dafuuuuuq":
-    print("I'm leaving, forgive me ...")
-    time.sleep(3)
-
-# there is more fun in this code, the full version is vol.1
